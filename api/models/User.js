e13136e7e9f261918f0cfd05201bd249179e7b34
Ramit <ramitsarkar902@gmail.com>
2021-10-27 22:47:12 +0530

    user model

---
+++ api/models/User.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      min: 3,
+      max: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      max: 50,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        "https://t4.ftcdn.net/jpg/00/97/00/09/360_F_97000908_wwH2goIihwrMoeV9QF3BW6HtpsVFaNVM.jpg",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+module.exports = mongoose.model("User", UserSchema);
