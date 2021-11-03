6f8ea559ad6210c09cd5ec50cbf12c8d2ccc7216
Ramit <ramitsarkar902@gmail.com>
2021-11-03 23:21:13 +0530

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,129 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-  res.send("user route");
+//update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch {
+        return res.status(500).json("Password error");
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (error) {
+      return res.status(500).json("user id error");
+    }
+  } else {
+    return res.status(403).json("You cannot update others!");
+  }
+});
+
+//delete user modified version using password
+/* router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    const user = await User.findById(req.params.id);
+    const storedPw = user.password;
+    bcrypt.compare(req.body.password, storedPw, async function (err, response) {
+      if (response) {
+        try {
+          await User.findByIdAndDelete(req.params.id);
+          res.status(200).json("Account has been deleted");
+        } catch (err) {
+          return res.status(500).json(err);
+        }
+      } else {
+        res.status(401).json("Unauthorized or password wrong");
+      }
+    });
+  } else {
+    return res.status(403).json("User id wrong");
+  }
+}); */
+
+//delete user using id only
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account!");
+  }
+});
+//get a user
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//follow a user
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+        res.status(200).json("user has been followed");
+      } else {
+        res.status(403).json("you already follow this user");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you cant follow yourself");
+  }
+});
+//unfollow a user
+
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { followings: req.params.id } });
+        res.status(200).json("user has been unfollowed");
+      } else {
+        res.status(403).json("you dont follow this user");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you cant unfollow yourself");
+  }
+});
+
+//get all users
+router.get("/", async (req, res) => {
+  try {
+    await User.find({}).then(function (users) {
+      res.send(users);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
