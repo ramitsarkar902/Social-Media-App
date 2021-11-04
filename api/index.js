fcad8ef03bdc0be9f926afaca26d62d1cbcf72e5
Ramit <ramitsarkar902@gmail.com>
2021-11-04 18:39:03 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -26,6 +27,7 @@ app.use(morgan("common"));
 
 app.use(`${API_URL}users`, userRoute);
 app.use(`${API_URL}auth`, authRoute);
+app.use(`${API_URL}posts`, postRoute);
 
 app.listen(`${process.env.PORT}`, () => {
   console.log("Server Running");
