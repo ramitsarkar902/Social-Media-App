0951f1d18bb144af8d487dffc6439840fa655809
Ramit <ramitsarkar902@gmail.com>
2021-11-15 16:18:56 +0530

    home page

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Topbar from "../../components/topbar/Topbar";
+import "./home.css";
+
+function Home() {
+  return (
+    <div>
+      <Topbar />
+    </div>
+  );
+}
+
+export default Home;
