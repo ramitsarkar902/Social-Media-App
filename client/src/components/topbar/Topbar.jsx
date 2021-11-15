bca40a6db49f656e90323733ff0abb20362575c6
Ramit <ramitsarkar902@gmail.com>
2021-11-15 16:18:46 +0530

    topbar component

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -2,8 +2,44 @@ import React from "react";
 import "./topbar.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
 function Topbar() {
-  return <div></div>;
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">DotBook</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for a friend or post"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Home</span>
+          <span className="topbarLink">TimeLine</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">4</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">3</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Topbar;
