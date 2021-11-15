8eac5aca78bbbf033c1369f62224ed7bc47558e5
Ramit <ramitsarkar902@gmail.com>
2021-11-15 07:59:44 +0530

    client create

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { useContext } from "react";
+
+function App() {
+  return <>hello</>;
+}
+
+export default App;
