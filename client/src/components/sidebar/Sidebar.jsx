31ca0d813aa1f2f2531c696e194f9e34508ac7d9
Ramit <ramitsarkar902@gmail.com>
2021-11-22 11:53:30 +0530

    sidebar

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Users } from "../../dummydata";
+import {
+  RssFeed,
+  Chat,
+  PlayCircleFilledOutlined,
+  Group,
+  Bookmark,
+  HelpOutline,
+  WorkOutline,
+  Event,
+  School,
+} from "@material-ui/icons";
+import "./sidebar.css";
 
 function Sidebar() {
-  return <div></div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <RssFeed className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+            <Chat className="sidebarIcon" />
+            <span className="sidebarListItemText">Chats</span>
+          </li>
+          {/* other items */}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
