000465ff55230d889ce6f49427cf03cc2154ea68
Ramit <ramitsarkar902@gmail.com>
2021-12-04 01:35:22 +0530

    testing using test page

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,12 @@ import { useContext } from "react";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Sidebar from "./components/sidebar/Sidebar";
+import Test from "./pages/Test";
 
 function App() {
   return (
     <>
-      <Sidebar />
+      <Test />
     </>
   );
 }
