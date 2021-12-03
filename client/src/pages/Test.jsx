f96473c00e99b71bd860c670ce6b220add0f74d6
Ramit <ramitsarkar902@gmail.com>
2021-12-04 01:35:32 +0530

    test page for random tests

---
+++ client/src/pages/Test.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Online from "../components/online/Online";
+import Rightbar from "../components/rightbar/Rightbar";
+function Test() {
+  return (
+    <div>
+      <Rightbar />
+    </div>
+  );
+}
+
+export default Test;
