85327c89121da2ff84d26f54db00704c93d90325
Ramit <ramitsarkar902@gmail.com>
2021-12-11 11:37:31 +0530

    edited the feeds component section and added styling. not responsive yet

--- client/src/components/feeds/Feed.jsx
+++ client/src/components/feeds/Feed.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./feed.css";
 import { Posts } from "../../dummydata.js";
 import Post from "../posts/Post";
+import Share from "../share/Share";
 function Feed() {
   return (
     <div className="feed">
       <div className="feedWrapper">
+        <Share />
         {Posts.map((item, index) => (
           <Post key={index} item={item} />
         ))}
