1cb5e46837ad37e83b3e9e95277c7ba8994b15f6
Ramit <ramitsarkar902@gmail.com>
2021-12-16 00:12:43 +0530

    modified

--- client/src/components/feeds/Feed.jsx
+++ client/src/components/feeds/Feed.jsx
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
 import "./feed.css";
-import { Posts } from "../../dummydata.js";
 import Post from "../posts/Post";
 import axios from "axios";
 import Share from "../share/Share";
-function Feed() {
+function Feed({ username }) {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const getFeedPosts = async () => {
-      const res = await axios.get("posts/timeline/6183cb70cd3c7299f6f7de02");
+      const res = username
+        ? await axios.get("/posts/profile/" + username)
+        : await axios.get("posts/timeline/6183cb70cd3c7299f6f7de02");
       setPosts(res.data);
     };
     getFeedPosts();
-  }, []);
+  }, [username]);
   return (
     <div className="feed">
       <div className="feedWrapper">
