71b1ee9f134bfa5a63e2ad186e21a12d5b2b2d24
Ramit <ramitsarkar902@gmail.com>
2021-12-15 17:30:26 +0530

    added the axios to fetch posts

--- client/src/components/posts/Post.jsx
+++ client/src/components/posts/Post.jsx
@@ -2,26 +2,33 @@ import React, { useState, useEffect } from "react";
 import "./post.css";
 import { Users } from "../../dummydata.js";
 import { MoreVert } from "@material-ui/icons";
-
+import axios from "axios";
+import { format } from "timeago.js";
+import { Link } from "react-router-dom";
 function Post({ item }) {
+  const [like, setLike] = useState(item.likes.length);
+  const [user, setUser] = useState({});
+
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`users/${item.userId}`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, [item.userId]);
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img
-              className="profileImage"
-              src={
-                Users.filter((user) => user.id === item?.userId)[0]
-                  .profilePicture
-              }
-              alt="hello"
-            />
-            <span className="postUsername">
-              {Users.filter((user) => user.id === item?.userId)[0].username}
-            </span>
-            <span className="postDate">{item.date}</span>
+            <Link to={`profile/${user.username}`}>
+              <img className="profileImage" src={user.profilePicture} alt="" />
+            </Link>
+
+            <span className="postUsername">{user.username}</span>
+            <span className="postDate">{format(item.createdAt)}</span>
           </div>
           <div className="postTopRight">
             <MoreVert className="icon" />
@@ -29,14 +36,14 @@ function Post({ item }) {
         </div>
         <div className="postCenter">
           <span className="postText">{item?.desc}</span>
-          <img className="postImg" src={PF + item.photo} alt="" />
+          <img className="postImg" src={PF + item.img} alt="" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <img className="likeIcon" src={`${PF}like.png`} alt="" />
             <img className="likeIcon" src={`${PF}heart.png`} alt="" />
 
-            <span className="postLikeCounter">{item.like} likes</span>
+            <span className="postLikeCounter">{like} likes</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{item.comment} comments</span>
