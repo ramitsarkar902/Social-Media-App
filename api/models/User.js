124c71b9818e8949f6f03de5846242a948cbf511
Ramit <ramitsarkar902@gmail.com>
2021-12-16 00:36:31 +0530

    chnaged user model relationship type to string

--- api/models/User.js
+++ api/models/User.js
@@ -54,8 +54,8 @@ const UserSchema = new mongoose.Schema(
       max: 50,
     },
     relationship: {
-      type: Number,
-      enum: [1, 2, 3],
+      type: String,
+      max: 20,
     },
   },
   {
