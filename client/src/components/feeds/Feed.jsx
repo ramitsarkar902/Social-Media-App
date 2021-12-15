cacaa896c45f0ff69c44608174ef056e2f7ece63
Ramit <ramitsarkar902@gmail.com>
2021-12-15 17:31:57 +0530

    added the get feed posts..{user not dynamic still}

--- client/src/components/feeds/Feed.jsx
+++ client/src/components/feeds/Feed.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./feed.css";
 import { Posts } from "../../dummydata.js";
 import Post from "../posts/Post";
+import axios from "axios";
 import Share from "../share/Share";
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getFeedPosts = async () => {
+      const res = await axios.get("posts/timeline/6183cb70cd3c7299f6f7de02");
+      setPosts(res.data);
+    };
+    getFeedPosts();
+  }, []);
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        {Posts.map((item, index) => (
+        {posts.map((item, index) => (
           <Post key={index} item={item} />
         ))}
       </div>
