39690e0c4e28f2e5b46fbe291735f649c4efd14d
Ramit <ramitsarkar902@gmail.com>
2021-12-16 23:04:22 +0530

    added the manual delay to axios data fetch using the wait func above

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -1,11 +1,18 @@
 import axios from "axios";
 
-export const loginCall = async (userCred, dispatch) => {
+function wait(ms, value) {
+  return new Promise((resolve) => setTimeout(resolve, ms, value));
+}
+export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post("auth/login");
+    const res = await axios
+      .post("/auth/login", userCredential)
+      .then((value) =>
+        wait(1000, value)
+      ); /* added the manual delay to axios data fetch using the wait func above */
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
-  } catch (error) {
-    dispatch({ type: "LOGIN_FAILURE", payload: error });
+  } catch (err) {
+    dispatch({ type: "LOGIN_FAILURE", payload: err });
   }
 };
