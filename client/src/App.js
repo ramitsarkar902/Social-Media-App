ac0bfbf9ee6a17c3e7e7cc6af5a7bdc801e4025c
Ramit <ramitsarkar902@gmail.com>
2021-12-24 01:39:20 +0530

    added edirect and logic for login/home/register

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,17 +12,17 @@ import Test from "./pages/Test";
 import Login from "./pages/login/Login";
 import "./app.css";
 import Register from "./pages/register/Register";
-
+import { AuthContext } from "./context/AuthContext.js";
 function App() {
+  const { user } = useContext(AuthContext);
+
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Home />
+          {user ? <Home /> : <Register />}
         </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
         <Route path="/register">
           <Register />
         </Route>
